8b14a425aaa3d7b8b873044eaa58826731502e4e
jnthnclt <jnthnclt@users.noreply.github.com>
2015-12-01 07:20:54 -1000

    Create push-javadoc-to-gh-pages.sh

---
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R build/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/jivesoftware/mlogger/wiki master > /dev/null
+
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to wiki"
+  git push -fq origin master > /dev/null
+
+  echo -e "Published Javadoc to wiki.\n"
+  
+fi
